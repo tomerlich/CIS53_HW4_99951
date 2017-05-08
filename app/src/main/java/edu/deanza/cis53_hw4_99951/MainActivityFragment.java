26680aa2e30b8fef14b4aab985de6cccea0b0db2
tomerlich <tomer.erlich2@gmail.com>
2017-05-08 10:51:29 -0700

    The project is now complete

--- app/src/main/java/edu/deanza/cis53_hw4_99951/MainActivityFragment.java
+++ app/src/main/java/edu/deanza/cis53_hw4_99951/MainActivityFragment.java
@@ -1,19 +1,16 @@
 package edu.deanza.cis53_hw4_99951;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
